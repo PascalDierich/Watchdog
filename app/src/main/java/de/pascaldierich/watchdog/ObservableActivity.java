917fc7f7550bddcdb4bdf8fa3da660696bc9b15f
Pascal Dierich <dierich.pascal@gmail.com>
2017-02-09 15:22:40 +0100

    Watchdog initial commit

---
+++ app/src/main/java/de/pascaldierich/watchdog/ObservableActivity.java
@@ -0,0 +1,13 @@
+package de.pascaldierich.watchdog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ObservableActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observable);
+    }
+}
