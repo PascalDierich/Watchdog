f1465c7a6426a00a9fd1cb2e0c9c392cf555cdcf
Pascal Dierich <dierich.pascal@gmail.com>
2017-03-10 18:43:47 +0100

    feat: add toolbar

--- app/src/main/java/de/pascaldierich/watchdog/ui/activities/MainActivity.java
+++ app/src/main/java/de/pascaldierich/watchdog/ui/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
     // Layout
     @BindView(R.id.fab_newObservable)
     FloatingActionButton mFab;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
     
     /*
         initial Methods
@@ -51,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements MainPresenter.Vie
         setContentView(R.layout.activity_main);
         
         ButterKnife.bind(this);
+        
+        setSupportActionBar(mToolbar);
         
         mPresenter = Presenter.onCreate(ThreadExecutor.getInstance(), MainThreadImpl.getInstance(),
                 savedInstanceState, this);
